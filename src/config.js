3c5bd3c5017f076f7ec90cfe633c2dcdca48b0d5
Emin Muhammadi <muemin17631@sabah.edu.az>
2020-07-31 15:08:10 +0400

    Analytics added

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     API_URL : 'https://api-goldengate.emiga.tech',
-    GRecaptcha: '6LcZKrYZAAAAAP8aja68gPBRX-6LWaFpmvJSBLyL',
+    GRecaptcha: '6LfKlbgZAAAAAC42NCaaoKPY8IC99QrU3-QQ7rqf',
     GaTag: 'UA-172785494-1',
     GaDebug: false
 };
